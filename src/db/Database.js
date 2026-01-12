6f9768f6fc88a4a5b6815e1b9dc4d5918e5a1b20
Jakub Špernoga <82532980+Its1akub@users.noreply.github.com>
2026-01-12 11:03:17 +0100

    Update Database.js

--- src/db/Database.js
+++ src/db/Database.js
@@ -8,7 +8,8 @@ const pool = mysql.createPool({
     ...config,
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0
+    queueLimit: 0,
+    multipleStatements: true
 });
 
 module.exports = {
